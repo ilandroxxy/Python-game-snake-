e6754bb052dab9c7a27ad94c3ee8b3bd981cf6d6
ilandroxxy <ilyand98@mail.ru>
2019-04-28 17:49:51 +0700

    Finished programm

--- main.cpp
+++ main.cpp
@@ -1,23 +1,16 @@
 #include <SFML/Graphics.hpp>
-#include <time.h>
-#include <conio.h>
 #include <cstdlib>
+#include <time.h>
 using namespace sf;
 
-int N = 30, M = 20;
-int size = 32;
-int w = size * N;
-int h = size * M;
 float delay = 0.15;
-int L = 1, R = 1, U = 1, D = 1;
-
-int dir, num = 2, max = 0, spe = 1, max2 = 0;
+int dir, num = 2, max1 = 0, spe = 1, max2 = 0;
 
 struct Snake
 {
 	int x;
 	int y;
-}  s[100];
+} s[100];
 
 struct Fruct
 {
@@ -27,22 +20,30 @@ struct Fruct
 
 void Tick()
 {
+	int N = 30, M = 20;
+
 	for (int i = num; i > 0; --i)
 	{
 		s[i].x = s[i - 1].x;
 		s[i].y = s[i - 1].y;
 	}
 
-	if (dir == 0) s[0].y += 1;
-	if (dir == 1) s[0].x -= 1;
-	if (dir == 2) s[0].x += 1;
-	if (dir == 3) s[0].y -= 1;
+	if (dir == 0)
+		s[0].y += 1;
+	if (dir == 1)
+		s[0].x -= 1;
+	if (dir == 2)
+		s[0].x += 1;
+	if (dir == 3)
+		s[0].y -= 1;
 
 	if ((s[0].x == f.x) && (s[0].y == f.y))
 	{
 		num++;
-		if (num >= max) max = num;
-		if (num >= max2) max2 = num;
+		if (num >= max1)
+			max1 = num;
+		if (num >= max2)
+			max2 = num;
 
 		if (num > 5)
 		{
@@ -64,11 +65,14 @@ void Tick()
 		f.y = rand() % M;
 	}
 
-	if (s[0].x > N - 1) s[0].x = 0;
-	if (s[0].x < 0) s[0].x = N - 1;
-	if (s[0].y > M - 1) s[0].y = 0;
-	if (s[0].y < 0) s[0].y = M - 1;
-
+	if (s[0].x > N - 1)
+		s[0].x = 0;
+	if (s[0].x < 0)
+		s[0].x = N - 1;
+	if (s[0].y > M - 1)
+		s[0].y = 0;
+	if (s[0].y < 0)
+		s[0].y = M - 1;
 
 	for (int i = 1; i < num; i++)
 	{
@@ -82,16 +86,28 @@ void Tick()
 int main()
 {
 	srand(time(NULL));
-	RenderWindow window(VideoMode(w + 240, h), "Snake Game", Style::None);  //Style::Fullscreen  //Style::None
+
+	int N = 30, M = 20;
+	int size = 32;
+	int w = size * N;
+	int h = size * M;
+	int L = 1, R = 1, U = 1, D = 1;
+
+	RenderWindow window(
+		VideoMode(w + 240, h),
+		"Snake Game",
+		Style::None); // Style::Fullscreen  //Style::None
 
 	Texture t1, t2, t3;
-	t1.loadFromFile("images/77.png");
-	t2.loadFromFile("images/23.png");
-	t3.loadFromFile("images/666.png");
-	if (!t1.loadFromFile("images/77.png")) return 0;
-	if (!t2.loadFromFile("images/23.png")) return 0;
-	if (!t3.loadFromFile("images/666.png")) return 0;
-
+	t1.loadFromFile("/home/ilya/Документы/PythonGame/images/77.png");
+	t2.loadFromFile("/home/ilya/Документы/PythonGame/images/23.png");
+	t3.loadFromFile("/home/ilya/Документы/PythonGame/images/666.png");
+	if (!t1.loadFromFile("/home/ilya/Документы/PythonGame/images/77.png"))
+		return 0;
+	if (!t2.loadFromFile("/home/ilya/Документы/PythonGame/images/23.png"))
+		return 0;
+	if (!t3.loadFromFile("/home/ilya/Документы/PythonGame/images/666.png"))
+		return 0;
 
 	Sprite sprite1(t1);
 	Sprite sprite2(t2);
@@ -99,7 +115,6 @@ int main()
 
 	Clock clock;
 	float timer = 0;
-
 
 	f.x = 10;
 	f.y = 10;
@@ -119,35 +134,36 @@ int main()
 			}
 		}
 
-		if (Keyboard::isKeyPressed(Keyboard::Left) && R == 1)
+		if (Keyboard::isKeyPressed(Keyboard::A) && R == 1)
 		{
 			dir = 1;
 			L = 0;
 			U = 1;
 			D = 1;
 		}
-		if (Keyboard::isKeyPressed(Keyboard::Right) && L == 1)
+		if (Keyboard::isKeyPressed(Keyboard::D) && L == 1)
 		{
 			dir = 2;
 			R = 0;
 			U = 1;
 			D = 1;
 		}
-		if (Keyboard::isKeyPressed(Keyboard::Up) && D == 1)
+		if (Keyboard::isKeyPressed(Keyboard::W) && D == 1)
 		{
 			dir = 3;
 			U = 0;
 			L = 1;
 			R = 1;
 		}
-		if (Keyboard::isKeyPressed(Keyboard::Down) && U == 1)
+		if (Keyboard::isKeyPressed(Keyboard::S) && U == 1)
 		{
 			dir = 0;
 			D = 0;
 			L = 1;
 			R = 1;
 		}
-		if (Keyboard::isKeyPressed(Keyboard::Q)) return 0;
+		if (Keyboard::isKeyPressed(Keyboard::Q))
+			return 0;
 		if (Keyboard::isKeyPressed(Keyboard::E))
 		{
 			num = 2;
@@ -167,16 +183,17 @@ int main()
 		window.clear();
 
 		Font font;
-		font.loadFromFile("images/5555.ttf");
-		if (!font.loadFromFile("images/5555.ttf"))
+		font.loadFromFile("/home/ilya/Документы/PythonGame/images/5555.ttf");
+		if (!font.loadFromFile(
+				"/home/ilya/Документы/PythonGame/images/5555.ttf"))
 		{
 			return 0;
 		}
-		Text text, text0, text2, text3, text4, text5, text6, text7, text8, text9;
+		Text text, text0, text2, text3, text4, text5, text6, text7, text8,
+			text9;
 
 		if (num < max2)
 		{
-
 			text3.setFont(font);
 			text3.setString("Your Result:");
 			text3.setCharacterSize(24);
@@ -200,7 +217,6 @@ int main()
 			text9.setCharacterSize(65);
 			text9.setPosition(1017, 280);
 			window.draw(text9);
-
 
 			text5.setFont(font);
 			text5.setString("Press 'E' for restart");
@@ -242,7 +258,7 @@ int main()
 
 			text4.setFont(font);
 			text4.setPosition(1150, 170);
-			text4.setString(std::to_string(max));
+			text4.setString(std::to_string(max1));
 			text4.setCharacterSize(24);
 			window.draw(text4);
 
@@ -265,25 +281,23 @@ int main()
 			window.draw(text5);
 		}
 
-
 		for (int i = 0; i < N; i++)
 		{
 			for (int j = 0; j < M; j++)
 			{
-				sprite1.setPosition(i*size, j*size);
+				sprite1.setPosition(i * size, j * size);
 				window.draw(sprite1);
 			}
 		}
 
 		for (int i = 0; i < num; i++)
 		{
-			sprite2.setPosition(s[i].x*size, s[i].y*size);
+			sprite2.setPosition(s[i].x * size, s[i].y * size);
 			window.draw(sprite2);
 		}
 
-		sprite3.setPosition(f.x*size, f.y*size);
+		sprite3.setPosition(f.x * size, f.y * size);
 		window.draw(sprite3);
-
 
 		window.display();
 	}
